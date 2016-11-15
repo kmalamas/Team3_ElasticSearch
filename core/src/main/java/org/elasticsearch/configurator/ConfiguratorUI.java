a776400f39337f0075430cbfa6efe32d1fd3c423
kmalamas <kyr88@hotmail.com>
2016-11-15 21:51:22 +0100

    a very simple gui for the configurator so we can quickly enable/disable features

---
+++ core/src/main/java/org/elasticsearch/configurator/ConfiguratorUI.java
@@ -0,0 +1,71 @@
+package org.elasticsearch.configurator;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+/**
+ * Created by kmala on 15-Nov-16.
+ */
+public class ConfiguratorUI  extends JFrame implements java.awt.event.ActionListener, ItemListener{
+
+
+    private JCheckBox c1,c2,c3, c4;
+    private JPanel pane;
+    private JButton bt;
+    Configurator config;
+
+    public ConfiguratorUI() {
+
+    initialize();
+    }
+
+    private void initialize(){
+
+        config = Configurator.getInstance();
+        c1 = new JCheckBox("GeoKMeans Feature");
+        c2 = new JCheckBox("LateParsingQuery Feature");
+        c3 = new JCheckBox("UsageStatistics Feature");
+        c4 = new JCheckBox("XMLShit Feature");
+        pane = new JPanel();
+        bt = new JButton("Apply");
+        c1.addItemListener(this);
+        c2.addItemListener(this);
+        c3.addItemListener(this);
+        c4.addItemListener(this);
+        bt.addActionListener(this);
+
+
+        pane.add(c1);
+        pane.add(c2);
+        pane.add(c3);
+        pane.add(c4);
+        pane.add(bt);
+        this.add(pane);
+        pane.setPreferredSize(new Dimension(800,400));
+
+        c1.setSelected(config.getisGeoKMeansActivated());
+        c2.setSelected(config.getisLateParsingQueryActivated());
+        c3.setSelected(config.getisUsageStatisticsActivated());
+        c4.setSelected(config.getisXMLActivated());
+
+    }
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()==bt)
+        {
+            config.setGeoKMeansActivated(c1.isSelected());
+            config.setLateParsingQueryActivated(c2.isSelected());
+            config.setUsageStatisticsActivated(c3.isSelected());
+            config.setXMLActivated(c4.isSelected());
+            config.saveConfiguration();
+        }
+
+    }
+    public  void itemStateChanged(ItemEvent a){}
+
+
+
+}
