67c61ea671e6de4a1af48b203f7a0beaae64885a
kmalamas <kyr88@hotmail.com>
2016-11-15 21:51:53 +0100

    main method to run Configurator GUI

---
+++ core/src/main/java/org/elasticsearch/configurator/main.java
@@ -0,0 +1,13 @@
+package org.elasticsearch.configurator;
+
+import java.awt.*;
+
+public class main {
+    public static void main( String args[] )
+    {
+        // Create an instance of the test application
+        ConfiguratorUI g	= new ConfiguratorUI();
+        g.setSize(800,400);
+        g.setVisible( true );
+    }
+}
